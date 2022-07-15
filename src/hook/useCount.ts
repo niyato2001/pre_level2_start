f027b2036b25230c56a22c37fd7b38cacac4086c
niyato2001 <niyato2001@gmail.com>
2022-07-15 20:14:52 +0900

    useCountでuseCallbackを適応

--- src/hook/useCount.ts
+++ src/hook/useCount.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 interface CountObj {
   count: number;
@@ -20,17 +20,24 @@ export const useCount = (): UseCountReturnType => {
   const [countObj, setCountObj] = useState<CountObj>(initCountObj);
   const [countList, setCountList] = useState<number[]>([0]);
 
-  const countUp = (): void => setCount((count) => (count += 1));
-  const objCountUp = (): void => {
-    const newCountObj = { ...countObj };
-    // newCountObj = countObjではレンダリングされないのはなぜ？？
-    newCountObj.count++;
-    setCountObj(newCountObj);
-  };
-  const listCountUp = (): void => {
-    const newCountList = [...countList];
-    newCountList[0]++;
-    setCountList(newCountList);
-  };
+  const countUp = useCallback((): void => {
+    setCount((count) => (count += 1));
+  }, []);
+  const objCountUp = useCallback((): void => {
+    setCountObj((countObj) => {
+      const newCountObj = { ...countObj };
+      // newCountObj = countObjではレンダリングされないのはなぜ？？
+      // {...countObj}にすることで浅いコピーになる！
+      newCountObj.count++;
+      return newCountObj;
+    });
+  }, []);
+  const listCountUp = useCallback((): void => {
+    setCountList((countList) => {
+      const newCountList = [...countList];
+      newCountList[0]++;
+      return newCountList;
+    });
+  }, []);
   return { count, countObj, countList, countUp, objCountUp, listCountUp };
 };
