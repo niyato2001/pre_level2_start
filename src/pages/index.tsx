6d832e4772f66e0f1fa650abf0b0381309c8a627
niyato2001 <niyato2001@gmail.com>
2022-07-17 21:03:40 +0900

    memoを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import { Pattern1Memo } from '@/component/Pattern1';
-import { Pattern2Memo } from '@/component/Pattern2';
-import { Pattern3Memo } from '@/component/Pattern3';
-import { Pattern4Memo } from '@/component/Pattern4';
+import { Pattern1 } from '@/component/Pattern1';
+import { Pattern2 } from '@/component/Pattern2';
+import { Pattern3 } from '@/component/Pattern3';
+import { Pattern4 } from '@/component/Pattern4';
 
 const Home: React.FC = () => (
   <main className='mx-auto max-w-6xl py-6 text-primary-800'>
     <h1 className='text-2xl font-extrabold '>Rendering Optimization of React</h1>
     <div className='test-lg my-6 flex flex-col gap-4'>
-      <Pattern1Memo />
-      <Pattern2Memo />
-      <Pattern3Memo />
+      <Pattern1 />
+      <Pattern2 />
+      <Pattern3 />
     </div>
-    <Pattern4Memo />
+    <Pattern4 />
   </main>
 );
 export default Home;
