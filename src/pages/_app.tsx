7f524cf6ff748fb4e69c61067d9daf3796c88028
niyato2001 <niyato2001@gmail.com>
2022-07-17 10:45:16 +0900

    _app.tsxにRecoilRootを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app';
 import '@/style/globals.css';
+import { RecoilRoot } from 'recoil';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <RecoilRoot>
+    <Component {...pageProps} />
+  </RecoilRoot>
+);
 
 export default App;
